4bd315603521533e770d9f208290aad7d1855c9c
unknown <muhaazir9@gmail.com>
2020-10-23 23:03:00 +0700

    first commit

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -18,11 +18,6 @@ class HomeController extends Controller
         $this->middleware('auth');
     }
 
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
         $user_id = Auth::user()->id;
